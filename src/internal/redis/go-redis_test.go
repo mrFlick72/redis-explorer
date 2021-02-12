e723c24bb93ead45b5ed5695d9323736115ab04f
mrFlick72 <valerio.vaudi@gmail.com>
2021-02-12 01:02:22 +0100

    scenario failure tests

---
+++ src/internal/redis/go-redis_test.go
@@ -0,0 +1,37 @@
+package redis
+
+import (
+	"testing"
+)
+
+func TestGoRedisRepository_ConnectTo(t *testing.T) {
+	panic("TODO")
+}
+
+func TestGoRedisRepository_DeleteObjectFor(t *testing.T) {
+	panic("TODO")
+}
+
+func TestGoRedisRepository_DisconnectFrom(t *testing.T) {
+	panic("TODO")
+}
+
+func TestGoRedisRepository_FlushAllDatabases(t *testing.T) {
+	panic("TODO")
+}
+
+func TestGoRedisRepository_FlushDatabaseFor(t *testing.T) {
+	panic("TODO")
+}
+
+func TestGoRedisRepository_GetDatabases(t *testing.T) {
+	panic("TODO")
+}
+
+func TestGoRedisRepository_GetObjectsFor(t *testing.T) {
+	panic("TODO")
+}
+
+func TestGoRedisRepository_Save(t *testing.T) {
+	panic("TODO")
+}
